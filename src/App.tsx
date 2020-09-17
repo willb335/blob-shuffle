409aa58e756ee307573c598968a71bc6f1e149f6
Will Bashelor <wbashelor@gmail.com>
2020-09-17 10:37:10 -0400

    fractional odds

--- src/App.tsx
+++ src/App.tsx
@@ -3,167 +3,64 @@ import styled from 'styled-components';
 import * as blob from 'blobs/v2';
 
 import { Blob, BlobProps } from './Blob';
-import logo from './react.svg';
 
 export const BlobContainer = styled.div<{ size: number }>`
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-around;
-  align-items: center;
+  justify-content: flex-start;
+  align-items: flex-start;
   width: ${({ size }) => size && `${size}px`};
   height: ${({ size }) => size && `${size}px`};
-  outline: 1px solid blue;
+  margin: 20px;
 `;
 
 const Container = styled.div`
   display: flex;
-  justify-content: flex-start;
-  align-items: flex-start;
-  width: 100vw;
+  justify-content: center;
+  align-items: center;
   flex-wrap: wrap;
-  outline: 2px solid blue;
+  max-width: 100vw;
+  border: 2px solid green;
 `;
-
-function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
 
 function App() {
   const [blobs, setBlobs] = useState<BlobProps[]>([]);
-  const [count, setCount] = useState(0);
-  const [size, setSize] = useState(2048);
-  const [recentBlob, setRecentBlob] = useState<BlobProps | undefined>(
-    undefined
-  );
+  const [fills] = useState<string[]>(['#D93F4C', '#337FBD', '#228F67']);
 
   useEffect(() => {
-    Array.from(Array(size)).forEach((_, i) => {
-      createBlob(i);
-    });
+    createBlob([0.5, 0.5]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  function createBlob(index: number): void {
-    const seed = Math.random();
-    const generation = 1;
-    const path = blob.svgPath({
-      seed,
-      extraPoints: 8,
-      randomness: 4,
-      // size: Math.floor(Math.sqrt(size)) - Math.floor(Math.sqrt(128)),
-      size: Math.floor(Math.sqrt(8)),
-    });
-
-    const currentBlob = {
-      size: Math.floor(Math.sqrt(8)),
-      path,
-      id: `${index}-${generation}`,
-      fill: index === 0 ? 'red' : getRandomColor(),
-      generation: 1,
-      children: undefined,
-      index,
-    };
-
-    setBlobs((prev) => {
-      return [...prev, currentBlob];
-    });
-    setRecentBlob(currentBlob);
-    setCount((prev) => prev + 1);
-  }
-
-  function splitBlob(i: number): void {
-    let newBlobs = blobs;
-    let generation = newBlobs[i].generation + 1;
-    let size = newBlobs[i].size / generation;
-    let splitCount = Math.pow(generation, 2);
-
-    const createChildren = (index: number): BlobProps[] => {
-      return Array.from(Array(splitCount)).map((_, i) => {
+  function createBlob(odds: number[]): void {
+    odds.forEach((odd, i) => {
+      const createPath = (odd: number): string => {
         const seed = Math.random();
-
-        const path = blob.svgPath({
+        return blob.svgPath({
           seed,
           extraPoints: 8,
           randomness: 4,
-          size,
+          size: odd * 100,
         });
+      };
 
-        return {
-          generation,
-          size,
-          path,
-          id: `${i}-${generation}`,
-          fill: getRandomColor(),
-          children: undefined,
-          index,
-        };
-      });
-    };
-
-    newBlobs[i] = {
-      ...newBlobs[i],
-      ...{
-        generation,
-        children: createChildren(i),
-      },
-    };
-
-    setBlobs(newBlobs);
-    setSize(size);
-    setRecentBlob(newBlobs[i]);
-    setCount(
-      (prev) => prev + (Math.pow(generation, 2) - Math.pow(generation - 1, 2))
-    );
+      const path = createPath(odd);
+      const fill = fills[i];
+      const size = odd * 100;
+      setBlobs((prev) => [...prev, { path, size, fill }]);
+    });
   }
-
-  useEffect(() => {
-    console.log(size);
-  }, [size]);
 
   return (
     <>
-      <img src={logo} className="App-logo" alt="logo" />
-      <div>Count: {count}</div>
-      <button onClick={() => createBlob(blobs.length)}>Click Me</button>
-      <BlobContainer size={128}>
-        {recentBlob && <Blob {...recentBlob} />}
-      </BlobContainer>
       <Container>
-        <BlobContainer tabIndex={0} size={128}>
-          {blobs.map((blob, i) => {
-            return <Blob key={blob.id} {...blob} index={i} />;
-          })}
-        </BlobContainer>
-
-        {/* {blobs.map((blob, i) => {
+        {blobs.map((blob, i) => {
           return (
-            <BlobContainer
-              key={blob.id}
-              tabIndex={0}
-              size={128}
-              onClick={() => i !== 0 && splitBlob(i)}
-              onKeyPress={(e) => {
-                console.log('e.key', e.target);
-
-                if (e.key === ' ') {
-                  console.log('e.key', e.key);
-                  splitBlob(i);
-                }
-
-                if (e.key === 'Enter') {
-                  console.log('e.key', e.key);
-                  createBlob(blobs.length);
-                }
-              }}
-            >
-              <Blob {...blob} index={i} />
+            <BlobContainer key={blob.path} tabIndex={0} size={blob.size}>
+              <Blob {...blob} />
             </BlobContainer>
           );
-        })} */}
+        })}
       </Container>
     </>
   );
--- src/Blob.tsx
+++ src/Blob.tsx
@@ -1,30 +1,13 @@
 import React, { FunctionComponent } from 'react';
-// import styled from 'styled-components';
-
-import { BlobContainer } from './App';
 
 export interface BlobProps {
   size: number;
   path: string;
   fill: string;
-  id: string;
-  generation: number;
-  children: BlobProps[] | undefined;
-  index: number;
 }
 
 export const Blob: FunctionComponent<BlobProps> = (props) => {
-  return props.children ? (
-    <>
-      {props.children.map((blob, i) => {
-        return (
-          <BlobContainer key={blob.id} size={blob.size}>
-            <Blob {...blob} index={props.index} />
-          </BlobContainer>
-        );
-      })}
-    </>
-  ) : (
+  return (
     <svg width={props.size} height={props.size}>
       <path d={props.path} fill={props.fill} />
     </svg>
