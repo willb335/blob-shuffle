fe990d0df3d4f5383991ed43aa77378f4affb288
Will Bashelor <wbashelor@gmail.com>
2020-09-19 08:48:36 -0400

    Updates

---
+++ precache-manifest.c1a1c0357ab0fc5420bfb7b972d7c6c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da943f19c41d6373685a4b7e48d4c553",
+    "url": "/blob-shuffle/index.html"
+  },
+  {
+    "revision": "2a12d603152cd1556ddb",
+    "url": "/blob-shuffle/static/js/2.97ea8a3f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blob-shuffle/static/js/2.97ea8a3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cdffea084be64b2f6fb",
+    "url": "/blob-shuffle/static/js/main.ec93d716.chunk.js"
+  },
+  {
+    "revision": "85ca377ab3f8a51e3221",
+    "url": "/blob-shuffle/static/js/runtime-main.07e14fe8.js"
+  }
+]);
